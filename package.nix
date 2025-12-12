34053748938b64888636554bd45c281597f88cde
Diogo Correia <dv_correia@hotmail.com>
2025-12-12 10:32:46 +0000

    fix nix package vendor hash (#22)
    * fix: update nix package vendor hash
    
    * fix: package default version should be a string

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildGoModule,
-  version ? 0.0.0,
+  version ? "0.0.0",
   commitHash ? "unknown",
 }:
 
@@ -11,7 +11,7 @@ buildGoModule (finalAttrs: {
 
   src = ./.;
 
-  vendorHash = "sha256-Gwkse7dl/EYxHSibgc6PMGwtDhKiMJYFajwAICpluzw=";
+  vendorHash = "sha256-8pNpJ9brmL88CVbh9vlm/Cd4QSstZTIWYn5nFqfe2Xw=";
 
   ldflags = [
     "-s"
